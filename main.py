af4ab8287c748a58de0782ce37a9b8c49d7e7a47
tech <tech.doglas@gmail.com>
2025-03-07 13:47:07 -0500

    update: add a local port

--- main.py
+++ main.py
@@ -300,4 +300,5 @@ def delete_item(serial_number):
         return f"Error deleting item: {str(e)}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0',port=8081)
