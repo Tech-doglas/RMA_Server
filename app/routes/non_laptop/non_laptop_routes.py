9c158126d801a8fc5ca6574c242c31d488d0ccd8
DannyTan <tantszhang1@gmail.com>
2025-03-30 00:15:41 -0400

    update

--- app/routes/non_laptop/non_laptop_routes.py
+++ app/routes/non_laptop/non_laptop_routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template, request, redirect
 from app.models import get_db_connection
 
+from app.routes.non_laptop.non_laptop_item_routes import non_laptop_item_bp
+
 non_laptop_bp = Blueprint('non_laptop', __name__)
+non_laptop_bp.register_blueprint(non_laptop_item_bp, url_prefix='/item')
 
 @non_laptop_bp.route('/', methods=['GET', 'POST'])
 def show_RMA_non_laptop_sheet():
@@ -15,5 +18,21 @@ def show_RMA_non_laptop_sheet():
         results = [dict(zip(columns, row)) for row in data]
         conn.close()
         return render_template('non_laptop/non_laptop.html', items=results)
+    except Exception as e:
+        return f"Error: {str(e)}"
+    
+@non_laptop_bp.route('/non_laptop_input')
+def non_laptop_input():
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM RMA_user")
+        data = cursor.fetchall()
+
+        conn.close()
+
+        results = [row[0] for row in data]
+
+        return render_template('non_laptop/non_laptop_input.html', users=results)
     except Exception as e:
         return f"Error: {str(e)}"
