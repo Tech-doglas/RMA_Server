387c0d532adab963e1610558b7300580df0e0b82
Danny <tech.doglas@gmail.com>
2025-04-24 09:56:54 -0400

    updated

--- backend/config.py
+++ backend/config.py
@@ -2,8 +2,8 @@
 class Config:
     SQL_SERVER = "localhost\\SQLEXPRESS"
     DATABASE = "RMA"
-    UID = "rma"
-    PWD = "rma_admin"
+    UID = "rma1"
+    PWD = "rma_admin_123"
     DRIVER = "{ODBC Driver 18 for SQL Server}"
     TRUST_CERT = "yes"
     MODI_RMA_DIR = r'\\192.168.50.179\modi\RMA'
