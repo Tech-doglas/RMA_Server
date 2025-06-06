710b5494f794a32e39d10cf1b164ab3525196aa2
DannyTan <tantszhang1@gmail.com>
2025-06-06 01:05:17 -0400

    bug fixed

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,9 @@
 # config.py
 class Config:
-    # SQL_SERVER = "1Panel-mssql-oP05"
-    SQL_SERVER = "localhost\SQLEXPRESS"
+    SQL_SERVER = "1Panel-mssql-oP05"
     DATABASE = "RMA"
     UID = "rma"
-    PWD = "rma_admin1234"
-    # PWD = "p-+\I16e656`+jB"
+    PWD = "p-+\I16e656`+jB"
     DRIVER = "{ODBC Driver 18 for SQL Server}"
     TRUST_CERT = "yes"
     MODI_RMA_DIR = "/vol1/1001/RMA"
